0afca7cdbf0269c766ac5b0b608427c4a6c3d220
Amneet <harryamneet600@gmail.com>
2021-01-31 22:57:59 +0530

    Initial Commit -Currently I have to add the backend in it, I am commiting in this way

---
+++ app.js
@@ -0,0 +1,28 @@
+// //jshint esversion:6
+
+// const express = require("express");
+// path = require('path');
+// const bodyParser = require("body-parser");
+// // const ejs = require("ejs");
+// const request = require("request");
+// const app = express();
+
+// app.use(bodyParser.urlencoded({ extended: true }));
+
+// // app.set('view engine', 'ejs');
+
+// // app.use(bodyParser.urlencoded({ extended: true }));
+// // app.use(express.static("public"));
+// // app.use(express.static("app"));
+
+
+// app.get("/", function(req, res) {
+//     // res.render("home");
+//     res.sendFile(__dirname + "/phaser3-parcel-template/src/index.html");
+
+// });
+
+
+// app.listen(3000, function() {
+//     console.log("Server started on port 3000");
+// });
